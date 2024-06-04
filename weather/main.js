3f6304e686aff9adf9a3e97eb029d97d96b4bc2e
Arthur Akhmetov <artur.akhmetov.2008@gmail.com>
2024-06-04 15:17:16 +0300

    Update mini app weather v0.2

--- weather/main.js
+++ weather/main.js
@@ -1,32 +1,57 @@
-const url = `
+// api: https://open-meteo.com/
+
+const latitudeNode = document.querySelector('#latitude')
+const longitudeNode = document.querySelector('#longitude')
+const btnNode = document.querySelector('#submit')
+
+const resultNode = document.querySelector('.result')
+
+let url = `
 https://api.open-meteo.com/v1/forecast?
-latitude=34.052235&
-longitude=-118.243683&
+latitude=${latitudeNode.value}&
+longitude=${longitudeNode.value}&
 current=temperature_2m,is_day,wind_speed_10m
 `
 
-fetch(url)
-	.then((data) => {
-		return data.json()
-	})
-	.then((data) => {
-		renderWeather(data)
-		console.log(data)
-	})
+latitudeNode.addEventListener('input', updateUrl)
+longitudeNode.addEventListener('input', updateUrl)
+
+function updateUrl() {
+	if (-90 < +latitudeNode.value < 90 || -180 < longitudeNode < 180) {
+		return (url = `https://api.open-meteo.com/v1/forecast?latitude=${latitudeNode.value}&longitude=${longitudeNode.value}&current=temperature_2m,is_day,wind_speed_10m
+    `)
+	}
+	return alert('Error')
+}
+
+btnNode.onclick = () => {
+	try {
+		fetch(url)
+			.then((data) => {
+				return data.json()
+			})
+			.then((data) => {
+				renderWeather(data)
+			})
+	} catch (error) {
+		console.log(error)
+	}
+}
 
 function renderWeather(data) {
-	const latitude = data.latitude
-	const longitude = data.longitude
+	const latitudeData = data.latitude
+	const longitudeData = data.longitude
 	const currentData = data.current
 	const roundTemperature = Math.round(currentData.temperature_2m)
 
 	const result = `
-  <p>Широта: ${latitude}</p>
-  <p>Долгота: ${longitude}</p>
-  <p>Температура: ${roundTemperature}</p>
+  <p>Широта: ${latitudeData}</p>
+  <p>Долгота: ${longitudeData}</p>
+  <p>Температура: ${roundTemperature}&deg;C</p>
   <p>Сейчас: ${currentData.is_day === 0 ? 'Ночь' : 'День'}</p>
   `
-	document.body.insertAdjacentHTML('beforebegin', result)
-}
 
-console.log(new Date('iso8601'))
+	resultNode.innerHTML = ''
+
+	resultNode.innerHTML = result
+}
